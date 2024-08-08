33f5d3361cc90c670f8bcfcc328662747abbe21b
Isabella-mg <isabellamartinez@uadec.edu.mx>
2024-08-07 20:48:06 -0600

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,48 @@
+// Comentarios
+// alt + 39 ''
+
+// alert('hola desde un alert!');
+
+let nombre = "Isabella";
+
+const pi = 3.14;
+// string nombre ="Juan";
+// Tipos de datos 
+
+// No definido
+let noDefinido;
+// Número
+let cantidad = 50;
+// Cadena de texto o string
+let bebida = "agua";
+// Booleanos
+let activo = true;
+
+// Objetos
+// Persona: nombre, apellido, edad, peso
+let persona = {nombre : 'Aurora', apellido : 'Pinzón', edad : 20,};
+console.log(persona);
+// Arrays colección
+let personas = [
+    {nombre: 'Melanie', apellido : 'Matus', edad : 21},
+    {nombre: 'Mayra', apellido : 'Arias', edad : 19},
+    {nombre: 'Karla', apellido : 'Yulenni', edad : 22},
+    persona
+]
+console.log(personas);
+console.log(personas[2]);
+
+// Funciones JS
+// Listado de instrucciones 
+function suma(a, b) {
+    return a + b;
+}
+let resultadoDeSuma = suma (5, 3);
+console.log(resultadoDeSuma);
+
+function multiplicacion(a, b) {
+    return a * b
+}
+let resultado = multiplicacion (5, 7);
+console.log(resultado);
+
